5dee907e05848726d16c25adfacc70e48faaf5c7
Gabbi Arguello <a.gabbi@wustl.edu>
2024-11-12 11:21:56 -0600

    interactive component. when square button is clicked handleClick is called and console logs clicked

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
-export default function Board() {
+function Square({ value }) {
+  function handleClick() {
+    console.log('clicked!');
+  }
+
   return (
-    <>
-      <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
-      </div>
-      <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
-      </div>
-      <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
-      </div>
-    </>
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
   );
 }
