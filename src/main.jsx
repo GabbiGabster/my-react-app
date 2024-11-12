f79435c0760bc1bbb4db8175a92b125e3e75f0dc
Gabbi Arguello <a.gabbi@wustl.edu>
2024-11-12 11:48:13 -0600

    main

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
 import App from './App.jsx'
+import TeaInfo from './TeaInfo.jsx'
+import TeaList from './TeaList.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    {/* <TeaInfo />  */}
+    {/* <TeaList/> */}
   </StrictMode>,
 )
